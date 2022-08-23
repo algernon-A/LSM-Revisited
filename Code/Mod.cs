49044cd91f1492797e9347f71e42fa0bf8e404e4
algernon-A <aeneas@aloku.net>
2022-08-23 10:37:06 +1000

    Add game version check.

--- Code/Mod.cs
+++ Code/Mod.cs
@@ -5,8 +5,11 @@
 
 namespace LoadingScreenModRevisited
 {
+    using AlgernonCommons;
+    using AlgernonCommons.Notifications;
     using AlgernonCommons.Patching;
     using AlgernonCommons.Translation;
+    using ColossalFramework.UI;
     using ICities;
 
     /// <summary>
@@ -14,6 +17,8 @@ namespace LoadingScreenModRevisited
     /// </summary>
     public sealed class Mod : PatcherMod<OptionsPanel, Patcher>, IUserMod
     {
+        private readonly string _compatibleVersion = "1.14";
+
         /// <summary>
         /// Gets the mod's base display name (name only).
         /// </summary>
@@ -30,6 +35,37 @@ namespace LoadingScreenModRevisited
         public string Description => Translations.Translate("MOD_DESCRIPTION");
 
         /// <summary>
+        /// Called by the game when the mod is enabled.
+        /// </summary>
+        public override void OnEnabled()
+        {
+            // Disable mod if version isn't compatible.
+            if (!BuildConfig.applicationVersion.StartsWith(_compatibleVersion))
+            {
+                Logging.Error("invalid game version detected!");
+
+                // Display error message.
+                // First, check to see if UIView is ready.
+                if (UIView.GetAView() != null)
+                {
+                    // It's ready - attach the hook now.
+                    DisplayVersionError();
+                }
+                else
+                {
+                    // Otherwise, queue the hook for when the intro's finished loading.
+                    LoadingManager.instance.m_introLoaded += DisplayVersionError;
+                }
+
+                // Don't do anything else - no options panel hook, no Harmony patching.
+                return;
+            }
+
+            // All good - continue as normal.
+            base.OnEnabled();
+        }
+
+        /// <summary>
         /// Saves settings file.
         /// </summary>
         public override void SaveSettings() => LSMRSettings.Save();
@@ -38,5 +74,14 @@ namespace LoadingScreenModRevisited
         /// Loads settings file.
         /// </summary>
         public override void LoadSettings() => LSMRSettings.Load();
+
+        /// <summary>
+        /// Displays a version incompatibility error.
+        /// </summary>
+        private void DisplayVersionError()
+        {
+            ListNotification versionErrorNotification = NotificationBase.ShowNotification<ListNotification>();
+            versionErrorNotification.AddParas(Translations.Translate("WRONG_VERSION"), Translations.Translate("SHUT_DOWN"), BuildConfig.applicationVersion);
+        }
     }
 }
