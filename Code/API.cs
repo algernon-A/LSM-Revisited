1d5b4049b97edff70b6905c148c067bd1a08a9a3
algernon-A <aeneas@aloku.net>
2022-09-10 12:03:03 +1000

    Update API to restore GetMateral and GetMesh overloads with isLod parameter.

--- Code/API.cs
+++ Code/API.cs
@@ -20,7 +20,8 @@ namespace LSM
         public static bool IsActive => Instance<Sharing>.HasInstance;
 
         /// <summary>
-        /// Attemps to retrieve a shared material with the given checksum from the specified package.
+        /// Attempts to retrieve a shared material with the given checksum from the specified package.
+        /// Will first attempt main model materials, then LOD materials.
         /// </summary>
         /// <param name="package">Package containing the material.</param>
         /// <param name="checksum">Material checksum.</param>
@@ -28,12 +29,31 @@ namespace LSM
         public static Material GetMaterial(Package package, string checksum) => Instance<Sharing>.instance?.GetMaterial(checksum, package);
 
         /// <summary>
-        /// Attemps to retrieve a shared mesh with the given checksum from the specified package.
+        /// Attempts to retrieve a shared material with the given checksum from the specified package.
+        /// </summary>
+        /// <param name="package">Package containing the material.</param>
+        /// <param name="checksum">Material checksum.</param>
+        /// <param name="isMain">True requesting the main material, false if requesting a LOD material.</param>
+        /// <returns>Shared material (null if unsuccessful).</returns>
+        public static Material GetMaterial(Package package, string checksum, bool isMain) => Instance<Sharing>.instance?.GetMaterial(checksum, package, isMain);
+
+        /// <summary>
+        /// Attempts to retrieve a shared mesh with the given checksum from the specified package.
+        /// Will first attempt main model meshes, then LOD meshes.
         /// </summary>
         /// <param name="package">Package containing the mesh.</param>
         /// <param name="checksum">Mesh checksum.</param>
         /// <returns>Shared mesh (null if unsuccessful).</returns>
         public static Mesh GetMesh(Package package, string checksum) => Instance<Sharing>.instance?.GetMesh(checksum, package);
+
+        /// <summary>
+        /// Attempts to retrieve a shared mesh with the given checksum from the specified package.
+        /// </summary>
+        /// <param name="package">Package containing the mesh.</param>
+        /// <param name="checksum">Mesh checksum.</param>
+        /// <param name="isMain">True requesting the main maesh, false if requesting a LOD mesh.</param>
+        /// <returns>Shared mesh (null if unsuccessful).</returns>
+        public static Mesh GetMesh(Package package, string checksum, bool isMain) => Instance<Sharing>.instance?.GetMesh(checksum, package, isMain);
 
         /// <summary>
         /// Attempts to retrieve the package containing the provided network prefab.
